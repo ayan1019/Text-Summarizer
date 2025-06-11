b0bea0de5afcab0396049efdca8e13d8bc216430
Ayan <ayanyadav2619@gmail.com>
2025-06-11 17:33:08 +0530

    Data Ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Text-Summarizer-Project
+
+## Workflow
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update the configuration manager in src config
+5. Update the config
+6. Update the pipleline
+7. Update the main.py
+8. Update the man.py
+
---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+# 👇 Add src to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+
+STAGE_NAME = "Data Ingestion Startded"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
